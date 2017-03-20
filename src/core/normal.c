e35147340e69987481c551fc19c630b3bacb1f0c
hbock-42 <hbock@student.42.fr>
2017-03-20 15:50:46 +0100

    add normal disc

--- src/core/normal.c
+++ src/core/normal.c
@@ -25,6 +25,8 @@ t_vector		get_normal(t_ray *ray)
 		normal = get_normal_at_cylinder(ray, ray->hitpoint.object->object);
 	else if (ray->hitpoint.object->type == BOX)
 		normal = ray->hitpoint.normal;
+	else if (ray->hitpoint.object->type == DISC)
+		normal = ray->hitpoint.normal;
 	else if (ray->hitpoint.object->type == CONE)
 		normal = get_normal_at_cone(ray, ray->hitpoint.object->object);
 	else if (ray->hitpoint.object->type == TORE)

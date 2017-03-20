10d13bab15c0f94984538a2902bf65574ff6de79
hbock-42 <hbock@student.42.fr>
2017-03-20 16:05:45 +0100

    add a nl before hitdisc proto because I couldnt add this file without this trick

--- inc/prototypes/core/objects.h
+++ inc/prototypes/core/objects.h
@@ -10,6 +10,7 @@ float			hit_cone(t_cone *cone, t_ray *ray);
 float			hit_tore(t_tore *tore, t_ray *ray);
 float			hit_boloid(t_boloid *boloid, t_ray *ray);
 float           hit_box(t_box *box, t_ray *ray);
-float           hit_disc(t_disc *disc, t_ray *ray);
+
+float           hit_disc(t_disc *disc, t_ray *ray)
 
 #endif

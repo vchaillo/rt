c1cc027c0f5bba57a9e68644f06534a687050070
vchaillo <valentinchaillou89@gmail.com>
2017-03-23 01:08:34 +0100

    update script ffmpeg

--- doc/movies/generate_video.sh
+++ doc/movies/generate_video.sh
@@ -3,6 +3,5 @@
 framerate=25
 images=images/*.bmp
 time=`date +"%Y%m%e_%H%M%S"`
-ffmpeg=../../lib/ffmpeg/ffmpeg_Linux/bin/ffmpeg 
 
-cat $images | $ffmpeg -framerate $framerate -f image2pipe -i - $time.mkv
+cat $images | ffmpeg -framerate $framerate -f image2pipe -i - $time.mkv
